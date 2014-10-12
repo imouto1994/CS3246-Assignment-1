526e03f9ef4f95157859e3a7a85d87830df4c6d1
Youn <imouto1994@gmail.com>
2014-10-12 08:16:23 +0800

    Refactor code and add matlabcontrol lib

--- src/FindIO/VisualWordExtraction.java
+++ src/FindIO/VisualWordExtraction.java
@@ -5,12 +5,12 @@ import java.io.File;
 public class VisualWordExtraction {
 
     public static void main(String[] args){
-        initializeProcess();
+        initializeProcess("demolist.txt");
     }
 
-    public static void initializeProcess() {
+    public static void initializeProcess(String fileListName) {
         File file = new File("src/FindIO/Features/Visual Concept");
-        ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "start", "image_classification.exe", "demolist.txt");
+        ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "start", "image_classification.exe", fileListName);
         builder.directory(file);
         builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
         builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
